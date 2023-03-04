81393aebdcc532c85c6d543c4d75839b11a51e0e
Ryun Kim <ryunkim@Ryuns-MacBook-Pro.local>
2023-03-04 18:20:10 -0500

    Added boilerplate code for assisted mode--load cells only for now

--- include/hw/LoadSensor.h
+++ include/hw/LoadSensor.h
@@ -6,27 +6,41 @@
 // ---------------------------------------------------------------
 
 
-#ifndef LOADSENSOR_H_
-#define LOADSENSOR_H_
-
+#ifndef LOAD_SENSOR_H_
+#define LOAD_SENSOR_H_
 
 #include "../core/BaseApp.h"
-
+#include <HX711_ADC.h>
 
 namespace hermes {
 namespace hw {
 
 class LoadSensor {
 public:
-
+    LoadSensor(void);
+    void init(void);
+    void loop(void);
+    float getThrottle(void);
 
 private:
+    const int HX711_dout_front = 4;
+    const int HX711_sck_front = 5;
+    const int HX711_dout_back = 6;
+    const int HX711_sck_back = 7;
+    HX711_ADC mLoadCellFront;
+    HX711_ADC mLoadCellBack;
+    const float mMaxLoad = 1000.0; // Maximum expected load in grams
+    float mFrontLoad = 0.0;
+    float mBackLoad = 0.0;
+    float mThrottle = 0.0;
+    unsigned long mLastUpdateTime = 0;
+    const unsigned long mUpdateInterval = 100; // Time in milliseconds between updates
 
-
+    void updateLoadValues(void);
+    void calculateThrottle(void);
 };
 
 }
 }
 
-
-#endif // LOADSENSOR_H_
+#endif // LOAD_SENSOR_H_
--- src/hw/LoadSensor.cpp
+++ src/hw/LoadSensor.cpp
@@ -12,6 +12,50 @@
 namespace hermes {
 namespace hw {
 
+LoadSensor::LoadSensor(void) : mLoadCellFront(HX711_dout_front, HX711_sck_front), mLoadCellBack(HX711_dout_back, HX711_sck_back) {}
+
+void LoadSensor::init(void) {
+    mLoadCellFront.begin();
+    mLoadCellBack.begin();
+}
+
+void LoadSensor::loop(void) {
+    unsigned long currentTime = millis();
+    if (currentTime - mLastUpdateTime >= mUpdateInterval) {
+        mLastUpdateTime = currentTime;
+        updateLoadValues();
+        calculateThrottle();
+    }
+}
+
+void LoadSensor::updateLoadValues(void) {
+    // Read load cell values and convert to grams
+    mFrontLoad = mLoadCellFront.getData() * mMaxLoad;
+    mBackLoad = mLoadCellBack.getData() * mMaxLoad;
+}
+
+void LoadSensor::calculateThrottle(void) {
+    // Calculate the difference in load between front and back
+    float loadDiff = mFrontLoad - mBackLoad;
+
+    // If load difference is within tolerance, maintain previous throttle
+    if (abs(loadDiff) < 50.0) {
+        return;
+    }
+
+    // Calculate the throttle based on the load difference
+    mThrottle = map(loadDiff, -mMaxLoad, mMaxLoad, -255, 255);
+    mThrottle = constrain(mThrottle, -255, 255);
+
+    // Invert throttle if load is applied to back of skates
+    if (loadDiff < 0.0) {
+        mThrottle = -mThrottle;
+    }
+}
+
+float LoadSensor::getThrottle(void) {
+    return mThrottle;
+}
 
 }
 }
