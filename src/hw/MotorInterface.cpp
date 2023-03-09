a12a52bd466aab779d38138a1e3bca750c96c5a2
Ryun Kim <ryunkim@Ryuns-MacBook-Pro.local>
2023-03-09 16:50:30 -0500

    PID control

--- src/hw/MotorInterface.cpp
+++ src/hw/MotorInterface.cpp
@@ -7,6 +7,7 @@
 
 
 #include "../../include/hw/MotorInterface.h"
+#include "../../include/hw/LoadSensor.h"
 
 
 namespace hermes {
@@ -26,34 +27,35 @@ bool MotorInterface::init(void)
 
 void MotorInterface::loop(void)
 {
-	if (mMotorEnabled) {
-		if (mMotorOn) {
-			// Algorithm needed
+    if (mMotorEnabled) {
+        if (mMotorOn) {
+            // Calculate error between desired speed setpoint and current speed of the motor
+            float speed = (float)mPwmSignal / __UINT8_MAX__ * MAX_SPEED; // Convert PWM signal to speed (assuming linear relationship)
+            float error = mThrottle - speed;
 
-			// 
+            // Compute PID output
+            unsigned long current_time = millis();
+            float dt = (current_time - mLastTimeUpdated) / 1000.0; // Time elapsed since last loop iteration, in seconds
+            mIntegral += error * dt;
+            float derivative = (error - mPreviousError) / dt;
+            float output = mKp * error + mKi * mIntegral + mKd * derivative;
+            mPreviousError = error;
+            mLastTimeUpdated = current_time;
 
-			// 
-
-			// Ratio of throttle to speed
-			float ratio = mPwmSignal / mThrottle;
-
-			// Check the time
-			uint32_t time = millis();
-			if (time - mLastTimeUpdated > mIntervalOfIncrease) {
-				mLastTimeUpdated = time;
-				if (mPwmSignal < __UINT8_MAX__) {
-					mPwmSignal++;
-				}
-			}
-
-			// Write signal to motor
-			analogWrite(ESC_PWM_PIN, mPwmSignal);
-		} else {
-
-			// If motor is OFF write 0's
-			analogWrite(ESC_PWM_PIN, 0);
-		}
-	}
+            // Convert PID output to PWM signal (assuming linear relationship)
+            float throttle = output / MAX_SPEED * __UINT8_MAX__;
+            if (throttle > mThrottle) {
+                throttle = mThrottle; // Limit throttle to maximum value set by user
+            } else if (throttle < 0) {
+                throttle = 0; // Ensure throttle is positive
+            }
+            mPwmSignal = (uint8_t)throttle;
+            analogWrite(ESC_PWM_PIN, mPwmSignal);
+        } else {
+            // If motor is OFF, write 0's
+            analogWrite(ESC_PWM_PIN, 0);
+        }
+    }
 }
 
 void MotorInterface::onCriticalFault(const core::CriticalFault& criticalFault)
