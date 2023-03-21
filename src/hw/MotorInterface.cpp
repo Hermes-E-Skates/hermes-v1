d2b0be8931af93bae61526c5b8649f9845afc36a
Glenn Tipold <45105502+GlennTipold@users.noreply.github.com>
2023-03-21 14:08:17 -0400

    Update MotorInterface.cpp

--- src/hw/MotorInterface.cpp
+++ src/hw/MotorInterface.cpp
@@ -210,6 +210,7 @@ void MotorInterface::onHallEffectStateChange(Pin_t pin, int16_t state)
 	if (state == 1) {
 		mHallCounter++;
 		uint32_t delta = millis() - mLastTime;
+
 		if (mHallCounter % 7 == 0) {
 			//Serial.println(delta);
 			mLastTime = millis();
